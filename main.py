905b27db63bbfa0740613ef6fa92325f81f53770
Tarık Yayla <tarik.yayla33@gmail.com>
2018-08-24 20:08:06 +0300

    id eklendi

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import json
 link = "https://www.kitapyurdu.com/index.php?route=product/best_sellers&list_id=2&filter_in_stock=1&filter_in_stock=1&limit=100"
 
 class Kitap:
-    def __init__(self,kitap_adi,yazar_adi,yayinevi,kapak,desc,link,fiyat):
+    def __init__(self,id,kitap_adi,yazar_adi,yayinevi,kapak,desc,link,fiyat):
+        self.id = id
         self.kitap_adi = kitap_adi
         self.yazar_adi = yazar_adi
         self.yayinevi = yayinevi
@@ -20,7 +21,7 @@ soup = bs(req,"html.parser")
 kitaplar = soup.find_all("div",{"itemtype":"http://schema.org/Book"})
 
 array = []
-for kitap in kitaplar:
+for id,kitap in enumerate(kitaplar):
     try:
         cover_div = kitap.find("div",{"class":"cover"})
         link = cover_div.find("a").get("href")
@@ -30,7 +31,7 @@ for kitap in kitaplar:
         yazar_adi = kitap.find("div",{"class":"author"}).find("a").text.lstrip(' ')
         desc = kitap.find("div",{"class":"product-info"}).text
         fiyat = kitap.find("div",{"class":"price-new "}).find("span",{"class":"value"}).text.lstrip(' ')
-        array.append(Kitap(kitap_adi,yazar_adi,yayinevi,kapak,desc,link,fiyat))
+        array.append(Kitap(id,kitap_adi,yazar_adi,yayinevi,kapak,desc,link,fiyat))
     except Exception as e:
         pass
         #Bazı kitapların yazar kısmı boş.
